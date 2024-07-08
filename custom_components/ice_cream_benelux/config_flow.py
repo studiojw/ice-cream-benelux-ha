f92a39d4c6717b6d46bc21770392f257c686ae5b
studiojw <studiojustwonder@gmail.com>
2024-07-08 08:33:44 +0200

    Use home coordinations as default values in the config flow

--- custom_components/ice_cream_benelux/config_flow.py
+++ custom_components/ice_cream_benelux/config_flow.py
@@ -4,6 +4,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_LATITUDE
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.selector import (
     SelectSelector,
@@ -15,21 +16,24 @@ from .const import APP_NAME, COMPANIES, CONF_COMPANIES, CONF_LONGITUDE, DOMAIN
 
 company_list = [{"label": value, "value": key} for key, value in COMPANIES.items()]
 
-data_schema = vol.Schema(
-    {
-        vol.Required(CONF_LATITUDE): cv.positive_float,
-        vol.Required(CONF_LONGITUDE): cv.positive_float,
-        vol.Required(CONF_COMPANIES, default=[]): SelectSelector(
-            SelectSelectorConfig(
-                options=list(company_list),
-                mode=SelectSelectorMode.DROPDOWN,
-                multiple=True,
-                custom_value=True,
-                translation_key="companies",
-            )
-        ),
-    }
-)
+
+def get_data_schema(home_lat, home_lon):
+    """Get data schema."""
+    return vol.Schema(
+        {
+            vol.Required(CONF_LATITUDE, default=home_lat or 0): cv.positive_float,
+            vol.Required(CONF_LONGITUDE, default=home_lon or 0): cv.positive_float,
+            vol.Required(CONF_COMPANIES, default=[]): SelectSelector(
+                SelectSelectorConfig(
+                    options=list(company_list),
+                    mode=SelectSelectorMode.DROPDOWN,
+                    multiple=True,
+                    custom_value=True,
+                    translation_key="companies",
+                )
+            ),
+        }
+    )
 
 
 def validate_user_input(user_input, errors):
@@ -46,21 +50,22 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
         if user_input is None:
-            return self._show_form(user_input)
+            return self._show_form(user_input, self.hass)
 
         errors = {}
         validated_input = self._validate_user_input(user_input, errors)
 
         if errors:
-            return self._show_form(user_input, errors)
+            return self._show_form(user_input, self.hass, errors)
 
         unique_id = self._generate_unique_id(validated_input)
         self._abort_if_unique_id_configured(unique_id)
 
         return self.async_create_entry(title=APP_NAME, data=validated_input)
 
-    def _show_form(self, user_input=None, errors=None):
+    def _show_form(self, user_input, hass: HomeAssistant, errors=None):
         """Show the form to the user."""
+        data_schema = get_data_schema(hass.config.latitude, hass.config.longitude)
         return self.async_show_form(
             step_id="user", data_schema=data_schema, errors=errors or {}
         )
